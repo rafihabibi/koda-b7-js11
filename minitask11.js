d1331c287e4a2cf33114cc98c42e96fcc89dad8a
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-27 15:42:41 +0700

    Add files via upload

---
+++ minitask11.js
@@ -0,0 +1,15 @@
+function menampilkanNama(nama, waktuTunggu) {
+    return new Promise((resolve, reject) => {
+        const name = true;
+        setTimeout(() => {
+        if(name){
+            resolve("John")
+        }
+        }), 
+        
+    })
+}
+
+async function printName(name) {
+    
+}
