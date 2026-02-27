677010a23dfd9291780871ca105422b1948efbbf
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-27 18:03:40 +0700

    Refactor menampilkanNama function for async usage

--- minitask11.js
+++ minitask11.js
@@ -1,12 +1,42 @@
-function menampilkanNama(nama, waktu){
-    return new Promise((resolve, reject) => {
-        setTimeout( () => {
-            const name1 = await
-            nama("John")
-            waktu(1500)
-        });
-        
-    })
+function menampilkanNama(nama, waktuTunggu) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(nama)
+    },waktuTunggu);
+    
+  });
 }
+menampilkanNama("John", 1500)
+  .then((namaJohn) => {
+    console.log(namaJohn);
+    return menampilkanNama("Ed", 2000);
+  })
+  .then((namaEd) => {
+    console.log(namaEd);
+    return menampilkanNama("Jane", 500);
+  })
+  .then((namaJane) => {
+    console.log(namaJane);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-async
+async function jalankanAsync() {
+  try {
+     const nama1 = await
+     menampilkanNama("John", 1500);
+     console.log(nama1);
+     
+     const nama2 = await
+     menampilkanNama("Ed", 2000);
+     console.log(nama2);
+     
+     const nama3 = await
+     menampilkanNama("Jane", 500);
+     console.log(nama3);
+     
+  } catch(error) {
+    console.error(error);
+  }
+}
